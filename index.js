917751a1f8a72c03875fc906be621c328773cea3
mssj-11 <michael11.salgado10@gmail.com>
2023-10-07 17:10:01 -0600

    Server

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path');
 
+
+
 let initial_path = path.join(__dirname, "public");
+
 
 const app = express();
 app.use(express.static(initial_path));
@@ -9,6 +12,7 @@ app.use(express.static(initial_path));
 app.get('/', (req, res) => {
     res.sendFile(path.join(initial_path, "home.html"));
 });
+
 
 
 app.listen('3000', () => {
