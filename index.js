4c8c18f7388c38eeb03211c0c76b04450b64f364
mssj-11 <michael11.salgado10@gmail.com>
2023-10-07 18:02:05 -0600

    Server

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
 const express = require('express');
 const path = require('path');
-
+const fileupload = require('express-fileupload');
 
 
 let initial_path = path.join(__dirname, "public");
 
-
 const app = express();
 app.use(express.static(initial_path));
+app.use(fileupload());
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(initial_path, "home.html"));
 });
 
+app.get('/editor', (req, res) => {
+    res.sendFile(path.join(initial_path, "editor.html"));
+});
+
+app.get('/blog', (req, res) => {
+    res.sendFile(path.join(initial_path, "blog.html"));
+});
+
+app.post('/upload', (req, res) => {
+    let file = req.files.image;
+    let date = new Date();
+    //  Image name
+    let imageName = date.getDate() + date.getTime() + file.name;
+    //  Image upload path
+    let path = 'public/uploads/' + imageName;
+
+    //  Create upload
+    file.mv(path, (err, result) => {
+        if (err){
+            throw err;
+        } else{ //  Our Image upload path
+            res.json(`uploads/${imageName}`);
+        }
+    });
+});
 
 
 app.listen('3000', () => {
